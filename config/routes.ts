c74a9e618029885e0809d64d58a597c699668f77
GuoJiaqi <guojiaqi5946@Outlook.com>
2024-08-29 23:46:15 +0800

    图标管理页面开发

--- config/routes.ts
+++ config/routes.ts
@@ -2,6 +2,7 @@
   { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
   { path: '/', redirect: '/add_chart' },
   { path: '/add_chart', name: '智能分析', icon: 'DotChart', component: './AddChart' },
+  { path: '/my_chart', name: '我的图表', icon: 'Home', component: './MyChart' },
   {
     path: '/admin',
     icon: 'crown',
